5ce094fe0c95e93d7e78133af30ba2e4b63ce003
Wolf Os Official <99878778+WolfOs-Official@users.noreply.github.com>
2022-03-11 01:05:07 +0700

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -4,12 +4,15 @@ net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator @zenitsu /add >nul
 net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
 echo Pass: @zenitsu
-curl -o https://raw.githubusercontent.com/WolfOs-Official/a/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+
+curl -O https://raw.githubusercontent.com/WolfOs-Official/a/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/WolfOs-Official/a/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\npp.7.9.4.Installer.x64.exe" https://raw.githubusercontent.com/WolfOs-Official/a/main/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
 
